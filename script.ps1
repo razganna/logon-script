1a9c6e98d253c7a006431edf83f501042909a1d4
razganna <razganna@gmail.com>
2022-09-21 23:27:07 +0300

    upload

---
+++ script.ps1
@@ -0,0 +1,18 @@
+﻿$Begin = Get-Date -Date '12:00:00'
+$End = Get-Date -Date '23:59:59'
+
+$filter = @{
+  LogName='Security'
+  StartTime=$Begin 
+  EndTime=$End
+  ID=4624
+}
+
+$properties = @(
+@{n='User';e={$_.Properties[5].Value}},
+@{n='TimeStamp';e={$_.TimeCreated}}
+)
+
+Get-WinEvent -FilterHashtable $filter | Select-Object $properties
+
+# Можно добавить вывод в графическом виде - |Out-GridView
